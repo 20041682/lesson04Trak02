5889926d92a3b640904b0ac5e075bbb322f245e6
Alexandra <SHUKLINA_ALEX@MAIL.RU>
2022-11-28 22:33:33 +0300

    Задача решена

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
-n = input("Введите трехзначное число: 364");
-n = int(n);
- 
-a = n % 10;
-b = (n / 100) % 10;
-c = n % 100;
- 
-print("Сумма цифр числа: 364", a + b + c);
+Console.WriteLine ("Start");
+int value = 549;
+
+int a = value / 100;
+int b = (value / 10) % 10;
+int c = value % 10;
+
+int result = a + b + c;
+Console.WriteLine (result); 
+Console.WriteLine ("End");
 
